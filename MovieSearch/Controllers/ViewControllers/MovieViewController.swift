e960b7b768a9d842c1d94e34cf24dbeae7cb36fc
tittaporn <tittaporn_saelee@icloud.com>
2021-01-29 16:57:20 -0600

    finished at assessment time, need to work more on it

---
+++ MovieSearch/Controllers/ViewControllers/MovieViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MovieViewController.swift
+//  MovieSearch
+//
+//  Created by Lee McCormick on 1/29/21.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var ratingLebel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var movieSummaryTextView: UITextView!
+    
+    // MARK: - Properties
+    var movie: Movie?
+    
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    // MARK: - Helper Fuctions
+    func updateViews() {
+        guard let movie = movie else { return }
+        movieTitleLabel.text = movie.title
+        ratingLebel.text = "Rating : \(movie.rating)"
+        movieSummaryTextView.text = movie.summary
+        movieSummaryTextView.isEditable = false
+        fetchPosterImage(movie: movie)
+    }
+    
+    func fetchPosterImage(movie: Movie) {
+        guard let moviePoster = movie.poster else { return }
+        MovieController.fetchPoster(with: moviePoster) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let posterImage):
+                    self.posterImageView.image = posterImage
+                case .failure(let error):
+                    self.presentErrorToUser(localizedError: error)
+                }
+            }
+        }
+    }
+}
